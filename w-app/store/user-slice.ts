fbaaa43f95cf62697200677d1f3807adeaa5a24d
Ali Sheerzad <ali.sheerzad9@gmail.com>
2023-08-25 08:01:28 +0300

    Added async thunks for logging out & checking for loggedin user, adding most of the business logic there to keep our components lean

--- w-app/store/user-slice.ts
+++ w-app/store/user-slice.ts
@@ -1,9 +1,13 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { RootState } from "./index";
+
+const supabase = createClientComponentClient();
 
 export interface User {
   isLoggedin: boolean;
   id: string | null;
-  email: string | null;
+  email: string | undefined;
   firstname: string | null;
   lastname: string | null;
   profilePicUrl: string | null;
@@ -12,7 +16,7 @@ export interface User {
 const initState: User = {
   isLoggedin: false,
   id: null,
-  email: null,
+  email: undefined,
   firstname: null,
   lastname: null,
   profilePicUrl: null,
@@ -26,7 +30,49 @@ const userSlice = createSlice({
       return (state = action.payload);
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(
+      handleLogout.fulfilled,
+      (state, action: PayloadAction<any>) => {
+        // anything returned from createAsynchThunk, will be found from action.payload
+        console.log("state: ", state);
+        window.location.href = "/";
+      }
+    );
+  },
 });
+
+export const checkIsUserLoggedIn = createAsyncThunk<
+  any,
+  any,
+  { state: RootState }
+>("user/signedIn", async (_, { getState, dispatch }) => {
+  const { user } = getState();
+  if (user.id) return;
+  const { data, error } = await supabase.auth.getUser();
+
+  if (data.user) {
+    console.log("inside if, with respone: ", data);
+    const currUser: User = {
+      isLoggedin: true,
+      id: data.user.id,
+      email: data.user.email,
+      firstname: data.user.user_metadata["firstname"],
+      lastname: data.user.user_metadata["lastname"],
+      profilePicUrl: null,
+    };
+
+    dispatch(userActions.setUser(currUser));
+  }
+});
+
+export const handleLogout = createAsyncThunk(
+  "user/handleLogout",
+  async (_, thunkAPI) => {
+    await supabase.auth.signOut();
+    // thunkAPI.dispatch(userSlice.actions.logOut());
+  }
+);
 
 export const userActions = userSlice.actions;
 export default userSlice.reducer;
