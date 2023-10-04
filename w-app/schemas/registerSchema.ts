26ba549267adebd80b7d73fa0e96d1cd66854f41
Ali Sheerzad <ali.sheerzad9@gmail.com>
2023-10-04 16:53:08 +0300

    Added password to yup object, and implemented match function with regex which checks that the input contains only letters from a-ö

--- w-app/schemas/registerSchema.ts
+++ w-app/schemas/registerSchema.ts
@@ -3,10 +3,14 @@ import * as yup from "yup";
 export const registerSchema = yup.object().shape({
   firstname: yup
     .string()
+    .matches(/^[a-ö]+$/, "Nimessä ei saa olla numeroita")
+    .strict()
     .min(2, "Etunimen oltava vähintään 2 merkkiä")
     .required("Pakollinen kenttä"),
   lastname: yup
     .string()
+    .matches(/^[a-ö]+$/, "Nimessä ei saa olla numeroita")
+    .strict()
     .min(2, "Sukunimen oltava vähintään 2 merkkiä")
     .required("Pakollinen kenttä"),
   email: yup
@@ -22,4 +26,8 @@ export const registerSchema = yup.object().shape({
       "2005-01-01",
       "Sinun on oltava vähintään 18-vuotias luodaksesi profiili"
     ),
+  password: yup
+    .string()
+    .min(5, "Salasanan on oltava vähintään 5-merkin pituinen")
+    .required("Pakollinen kenttä"),
 });
