cbd47252ccf95feaa836e4ea35b5520ca435910e
Ali Sheerzad <ali.sheerzad9@gmail.com>
2024-04-06 14:28:46 +0300

    New accent color which we use atm in Spinner component

--- w-app/tailwind.config.js
+++ w-app/tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         primary: "#FDF2E9",
         secondary: "#FFCA9F",
+        accent: "#FFCA9F",
         "button-primary": "#e67e22",
       },
       backgroundImage: {
