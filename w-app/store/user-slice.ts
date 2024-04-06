32348ef53ab09613162bc6eb623e90c9c379f7b8
Ali Sheerzad <ali.sheerzad9@gmail.com>
2024-04-06 16:36:31 +0300

    Updated logic and added helper function, now we fetch userdata from profile table on the server side

--- w-app/store/user-slice.ts
+++ w-app/store/user-slice.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { RootState } from "./index";
+import { loadSpinnerActions } from "./loadspinner-slice";
 
 const supabase = createClientComponentClient();
 
@@ -31,48 +32,53 @@ const userSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(
-      handleLogout.fulfilled,
-      (state, action: PayloadAction<any>) => {
-        // anything returned from createAsynchThunk, will be found from action.payload
-        console.log("state: ", state);
-        window.location.href = "/";
-      }
-    );
+    builder.addCase(handleLogout.fulfilled, (state, action: PayloadAction<any>) => {
+      // anything returned from createAsynchThunk, will be found from action.payload
+      console.log("state: ", state);
+      window.location.href = "/";
+    });
   },
 });
 
-export const checkIsUserLoggedIn = createAsyncThunk<
-  any,
-  any,
-  { state: RootState }
->("user/signedIn", async (_, { getState, dispatch }) => {
+export const checkIsUserLoggedIn = createAsyncThunk<any, any, { state: RootState }>("user/signedIn", async (_, { getState, dispatch }) => {
   const { user } = getState();
   if (user.id) return;
   const { data, error } = await supabase.auth.getUser();
 
   if (data.user) {
-    console.log("inside if, with respone: ", data);
-    const currUser: User = {
-      isLoggedin: true,
-      id: data.user.id,
-      email: data.user.email,
-      firstname: data.user.user_metadata["firstname"],
-      lastname: data.user.user_metadata["lastname"],
-      profilePicUrl: null,
-    };
+    const currUser = await fetchUserWithIdFromDB(data.user.id);
 
     dispatch(userActions.setUser(currUser));
   }
 });
 
-export const handleLogout = createAsyncThunk(
-  "user/handleLogout",
-  async (_, thunkAPI) => {
-    await supabase.auth.signOut();
-    // thunkAPI.dispatch(userSlice.actions.logOut());
-  }
-);
+export const setUserById = createAsyncThunk("user/setUserById", async (userId: string, thunkAPI) => {
+  const user: User = await fetchUserWithIdFromDB(userId);
+
+  thunkAPI.dispatch(userActions.setUser(user));
+});
+
+const fetchUserWithIdFromDB = async (userId: string): Promise<User> => {
+  const { data: profile, error } = await supabase.from("profile").select("*").eq("id", userId).maybeSingle();
+
+  if (error) console.log("ERROR FETCHING USER WITH ID IN SLICE: ", error);
+
+  const user: User = {
+    isLoggedin: true,
+    id: profile.id,
+    email: profile.email,
+    firstname: profile.firstname,
+    lastname: profile.lastname,
+    profilePicUrl: profile.profile_image,
+  };
+  return user;
+};
+
+export const handleLogout = createAsyncThunk("user/handleLogout", async (_, thunkAPI) => {
+  thunkAPI.dispatch(loadSpinnerActions.showLoadSpinner());
+  await supabase.auth.signOut();
+  thunkAPI.dispatch(loadSpinnerActions.hideLoadSpinner());
+});
 
 export const userActions = userSlice.actions;
 export default userSlice.reducer;
